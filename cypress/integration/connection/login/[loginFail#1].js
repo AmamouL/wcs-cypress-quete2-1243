d28b96ebb8bb15efc75bd41505384c48271db820
Sylvain Viole <sylvainviole@gmail.com>
2021-04-15 18:37:37 +0200

    fix loginfail#1 + update readme.md

--- cypress/integration/connection/login/[loginFail#1].js
+++ cypress/integration/connection/login/[loginFail#1].js
@@ -1,5 +1,8 @@
+let faker = require("faker");
 import { When, Then, Before, And } from "cypress-cucumber-preprocessor/steps";
 import { registerDom } from "../../../domElements/register";
+
+const userPassword = faker.internet.password()
 
 const url = "https://preprod.backmarket.fr/register";
 const passInput = registerDom.login.passInput;
@@ -11,8 +14,11 @@ const invalidCredentialsPrompt = registerDom.login.invalidCredentialsPrompt;
 And("email is not valid", () => {
     cy.get(emailInput).type("wrongMail");
 });
-And("password is valid", () => {
-    cy.get(passInput).type("Every3sr!ght4");
+And("login password is valid", () => {
+    cy.get(passInput).type(userPassword);
+});
+And("login email is empty", () => {
+    cy.get(emailInput).clear();
 });
 When("he logs in", () => {
     cy.get(submitLoginBtn).click();
@@ -21,5 +27,8 @@ Then("he should not be able to log in", () => {
     cy.url().should("eq", url);
 });
 And("he should be prompted {string}", (text) => {
-    cy.get(invalidCredentialPrompt).should("contain.text", text);
+    cy.get(invalidEmailPrompt).should("contain.text", text);
+});
+And("login email input field should turn to red", () => {
+    cy.get(emailInput).parent().should('have.class', 'input-error')
 });
--- readme.md
+++ readme.md
@@ -1,26 +1,39 @@
 # This repo hosts a Cypress project for Wild Code School #
 
 **author : Sylvain Viole**
----
-This test suite uses cucumber.
 
-Scenarios are written in Gherkins in ```.feature```  files.
+---
+
+This test suite challenges preprod.blackmarket register page on basic scenarios for log in and signup.
+
+---
+
+## CI Actions : ##
+
+Tests are automatically launched at each push
+
+---
+
+## Dependencies : ##
+|Dependency|version|
+|:-|:-|
+|Cypress|`7.1.0`|
+|Cypress-cucumber-preprocessor| `4.0.3`|
+|faker| `5.5.3`|
+---
+Scenarios are written in Gherkins in `.feature`  files.
 
 Test scripts are in corresponding sub-folders.
-Common part of test scripts are in the ```common``` folder.
-
-
-
+Common part of test scripts are in the `common` folder.
 ---
 ## How to : ##
 
-- ```git clone https://github.com/sylvain-viole/wcs-cypress-quete2-1243```
-
-- ```cd wcs-cypress-quete2-1243```
-
-- ```npm install```
-
-- ```npm run cy:run```
+```
+git clone https://github.com/sylvain-viole/wcs-cypress-quete2-1243
+cd wcs-cypress-quete2-1243
+npm install
+npm run cy:run
+```
 
  
 
